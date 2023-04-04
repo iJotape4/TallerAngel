9f5606c4f83bac7bfb59ae420bc463b533ff7a00
AngelGCJ <angelgcj@gmail.com>
2023-04-04 08:14:40 -0500

    Angel 03

--- TallerAngel/Assets/Scenes/Example.cs
+++ TallerAngel/Assets/Scenes/Example.cs
@@ -7,7 +7,7 @@ public class Example : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        bool example = false;
     }
 
     // Update is called once per frame
